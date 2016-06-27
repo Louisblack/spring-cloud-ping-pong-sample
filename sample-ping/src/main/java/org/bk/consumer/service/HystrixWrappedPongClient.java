2b7f2ff4b35315ebb989f670945925e500ce2ea9
Louis Houghton <louis@louishoughton.com>
2016-06-27 21:11:24 +0100

    Yet more fixes to get app working

--- sample-ping/src/main/java/org/bk/consumer/service/HystrixWrappedPongClient.java
+++ sample-ping/src/main/java/org/bk/consumer/service/HystrixWrappedPongClient.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 public class HystrixWrappedPongClient implements PongClient {
 
     @Autowired
-//    @Qualifier("pongClient")
     private PongClient feignPongClient;
 
     @Override
